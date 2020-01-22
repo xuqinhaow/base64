e91b61815f918792293fbac90fb27e4724ead462
Alfred Klomp <git@alfredklomp.com>
2020-01-22 01:44:50 +0100

    wip osx ci

---
+++ test/ci/osx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ve
+
+export SSSE3_CFLAGS=-mssse3
+export SSE41_CFLAGS=-msse4.1
+export SSE42_CFLAGS=-msse4.2
+export AVX_CFLAGS=-mavx
+
+uname -a
+${TRAVIS_COMPILER} --version
+
+make
+make -C test
